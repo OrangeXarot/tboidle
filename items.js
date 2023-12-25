20652bfcb28a0910cc31826e78fb7566928de3ac
OrangeXarot <orangexarot@proton.me>
2023-12-25 12:53:45 +0100

    deleted old stuff

--- items.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const itemsData = require('./items.json'); 
-
-exports.getItemStuff = function (id) {
-    const item = itemsData.find(item => item.itemid === id);
-    return item
-};
